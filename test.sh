aec9053f7888c16166f1b985d054511a795f3c30
Harun Mustafa <harun.mustafa@inf.ethz.ch>
2018-02-10 23:12:58 +0100

    Added test cases. Replace these with gtest soon.

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TRIALDIR=test_cases
+METANNOT=./metannot
+
+for a in $TRIALDIR/*.annot; do
+    echo $a
+    if [[ -z $(TEST=1 $METANNOT $a | grep "Done") ]]; then
+        echo "fail normal"
+        echo
+    fi;
+    if [[ -z $(TEST=1 STEP=2 $METANNOT $a | grep "Done") ]]; then
+        echo "fail split"
+        echo
+    fi;
+    for b in $TRIALDIR/*.annot; do
+        if [[ -z $(TEST=1 $METANNOT $a $b | grep "Done") ]]; then
+            echo "fail merge $a $b"
+            echo
+        fi;
+    done
+done
